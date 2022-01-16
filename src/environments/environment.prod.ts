75f29ad9c4a07523ce55e06409263d93e942f0d0
Bryan <72945681+bryancodesjs@users.noreply.github.com>
2022-01-16 17:00:11 -0400

    deployed for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCj7SfOtqDoIpZlm1XsH2wRrb2d3krC_gQ",
+    authDomain: "oriontek-test.firebaseapp.com",
+    projectId: "oriontek-test",
+    storageBucket: "oriontek-test.appspot.com",
+    messagingSenderId: "453560142685",
+    appId: "1:453560142685:web:d14e5b0747900617d5b6b4"
+  }
 };
